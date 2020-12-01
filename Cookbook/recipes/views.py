135e04ec15c82281bb0e185ec8397ea2617038c0
benjaminsierr <65034700+benjaminsierr@users.noreply.github.com>
2020-12-01 15:31:54 -0700

    ingredients and create completed 100%
    create recipe and add ingredients should be working just fine with the expection of uploading images

--- Cookbook/recipes/views.py
+++ Cookbook/recipes/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.shortcuts import redirect
 from .models import Recipe, RecipeIngredient, RecipeType
 # Create your views here.
 def indexPageView(request):
@@ -10,24 +11,50 @@ def createRecipePageView(request):
     return render(request, 'recipes/create_recipe.html')
 
 def addRecipe(request):
-    recipe_title = request.GET['recipeTitle']
-    recipe_description = request.GET['recipeDescription']
-    recipe_steps = request.GET['recipeSteps']
-    recipe_type = request.GET['recipeType']
-
+    recipe_title = request.POST.get('recipeTitle')
+    recipe_description = request.POST.get('recipeDescription')
+    recipe_steps = request.POST.get('recipeSteps')
+    recipe_type = request.POST.get('recipeType')
+    photo = request.POST.get('photo')
     rt = RecipeType.objects.get(recipe_type_description = recipe_type)
-    
-    recipe = Recipe(recipe_name = recipe_title, recipe_description = recipe_description, recipe_steps = recipe_steps, recipe_type = rt)
-    recipe.save()
-    print('Test')
 
-    return render(request, 'recipes/add_ingredients.html')
+    recipe = Recipe(recipe_name = recipe_title, recipe_description = recipe_description, recipe_steps = recipe_steps, recipe_type = rt,photo=photo)
+    recipe.save()
+    print(recipe.id)
+    return redirect('ingredients', recipe.id)
 
 def editRecipePageView(request):
     return render(request, 'recipes/edit_recipe.html')
 
-def addIngredientPageView(request):
-    return render(request, 'recipes/add_ingredients.html')
+def ingredientsPageView(request, recipe):
+    recipe_ingredients = []
+    for i in RecipeIngredient.objects.filter(recipe = recipe):
+        recipe_ingredients.append(i)
+    
+    context = {
+        'recipe' : Recipe.objects.get(id=recipe),
+        'ingredients' : recipe_ingredients,
+
+    }
+    return render(request, 'recipes/add_ingredients.html', context)
+
+def addIngredient(request, recipe):
+    ingredientName = request.POST.get('IngredientName')
+    measurementAmount = request.POST.get('MeasurementAmount')
+    measurementType = request.POST.get('MeasurementType')
+    r = Recipe.objects.get(id = recipe)
+    print(recipe)
+    ingredient = RecipeIngredient(ingredient_name=ingredientName,measure_amount=measurementAmount,measurement_type=measurementType, recipe=r)
+    ingredient.save()
+
+    return redirect('ingredients', recipe)
+
+def deleteIngredient(request, recipe):
+    print('----------',request.POST.get('ingredient'))
+    RecipeIngredient.objects.get(id = request.POST.get('ingredient')).delete()
+
+    return redirect('ingredients', recipe)
+
 
 def viewRecipePageView(request):
     return render(request, 'recipes/view_recipe.html')
