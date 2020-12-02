05a69e2be2ca1646ef62ab8084457226ea03ffee
benjaminsierr <65034700+benjaminsierr@users.noreply.github.com>
2020-12-02 14:04:03 -0700

    about - last commit?

--- Cookbook/recipes/models.py
+++ Cookbook/recipes/models.py
@@ -2,18 +2,19 @@ from django.db import models
 from django.db.models.deletion import CASCADE, DO_NOTHING
 from django.db.models.fields.related import ForeignKey
 
-# Create your models here.
+#Create your models here.
 
-# class User(models.Model):
-#     first_name = models.CharField(max_length=30)
-#     last_name = models.CharField(max_length=30)
-#     skill_level = models.CharField(max_length=30)
-#     email = models.EmailField(max_length=50)
-#     password = models.CharField(max_length=20)
-#     photo = models.ImageField(upload_to = 'photos', blank=True)
+#We created the user model before security was cut. We're just gonna leave it here so you know we have what it takes.
+class User(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    skill_level = models.CharField(max_length=30)
+    email = models.EmailField(max_length=50)
+    password = models.CharField(max_length=20)
+    photo = models.ImageField(upload_to = 'photos', blank=True)
 
-#     def __str__(self):
-#         return (self.first_name + ' ' + self.last_name)
+    def __str__(self):
+        return (self.first_name + ' ' + self.last_name)
 
 class RecipeType(models.Model):
     recipe_type_description = models.CharField(max_length=50)
